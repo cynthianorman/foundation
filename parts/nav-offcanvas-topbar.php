9b63a37783fb8f12d47310e09532a53efdb3d46d
Cynthia Norman <info@cynthianorman.com>
2020-03-27 06:33:41 -0400

    Initial Commit
    Some CSS styles and index.php changes

---
+++ parts/nav-offcanvas-topbar.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The off-canvas menu uses the Off-Canvas Component
+ *
+ * For more info: http://jointswp.com/docs/off-canvas-menu/
+ */
+?>
+
+<div class="top-bar" id="top-bar-menu">
+	<div class="top-bar show-for-medium">
+		<ul class="menu horizontal-large">
+			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
+			<li><a href="#">About Us</a></li>
+            <li><a href="#">Programs</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Campuses</a></li>
+            <li><a href="#">Blog</a></li>
+		</ul>
+	</div>
+	<div class="top-bar-right show-for-medium">
+		<?php joints_top_nav(); ?>	
+	</div>
+	<div class="top-bar-right float-right show-for-small-only">
+		<ul class="menu">
+			<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
+			<!-- <li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li> -->
+		</ul>
+	</div>
+</div>
